8c0c59349d1965962e7c97dcd6dc87a480b13cf9
Nikodim Lazarov <nlazarov@gmail.com>
2016-11-08 17:38:44 +0200

    Add first symlink install automation

--- install.sh
+++ install.sh
@@ -2,3 +2,17 @@
 
 #remap CAPS LOCK to Ctrl key
 setxkbmap -option caps:ctrl_modifier
+
+# creating symlinks
+echo "Setting up config symlinks"
+linkables=$( find -H ~/.dotfiles -type f -name *.ln )
+for $file in $linkables ; do
+    target="$HOME/.$( basename $file ".ln" )"
+    if [ -e $target ]; then
+        echo "~${$target#$HOME} already exists... Skipping."
+    else
+        echo "Symlink for $file"
+        ln -s $file $target
+    fi
+done
+
