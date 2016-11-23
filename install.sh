b26925cf81655310a5b143b4f3560a2bb4e6251c
Nikodim Lazarov <nlazarov@gmail.com>
2016-11-24 00:23:28 +0200

    fix install script errors

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 #remap CAPS LOCK to Ctrl key
-setxkbmap -option caps:ctrl_modifier
+if test $(which setxkbmap); then
+	setxkbmap -option caps:ctrl_modifier
+fi
 
 # creating symlinks
 echo "Setting up config symlinks"
 linkables=$( find -H ~/.dotfiles -type f -name *.ln )
-for $file in $linkables ; do
+for file in $linkables ; do
     target="$HOME/.$( basename $file ".ln" )"
     if [ -e $target ]; then
         echo "~${$target#$HOME} already exists... Skipping."
