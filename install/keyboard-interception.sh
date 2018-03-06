bb3e63ba88db3079859ae715d3d092137bd45104
Nikodim Lazarov <nlazarov@gmail.com>
2018-03-06 16:13:21 +0200

    Add software keyboard management
    Installation for making `CapsLock` useful:
    
    * tap will behave as `Esc`
    * long press will act as `Ctrl`

---
+++ install/keyboard-interception.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+git clone git@gitlab.com:interception/linux/tools.git
+cd tools
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+cd ../..
+
+git clone git@gitlab.com:interception/linux/plugins/caps2esc.git
+cd caps2esc
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+
+sudo cp "$(dirname $0)/keyboard/udevmon.yaml" /etc
+sudo cp "$(dirname $0)/keyboard/udevmon.service" /etc/systemd/system
+
+sudo systemctl enable udevmon
