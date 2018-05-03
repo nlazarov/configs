0c5e538fb5b9574fe0e6484daaa50645b11b3214
Nikodim Lazarov <nlazarov@gmail.com>
2018-05-03 20:13:31 +0300

    pump nvm version

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
