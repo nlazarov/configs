3d3a2b7e1b5249ed01972df84a4799f9df416070
Nikodim Lazarov <nlazarov@gmail.com>
2019-03-01 23:41:35 +0200

    Update nvm version

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
