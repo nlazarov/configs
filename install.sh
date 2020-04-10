f9c61df3ccbf33fe6bba18366b207048a608808f
Nikodim Lazarov <nlazarov@gmail.com>
2020-04-10 16:15:05 +0300

    Fixes thanks to shellcheck

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #remap CAPS LOCK to Ctrl key
-if test $(which setxkbmap); then
+if test "$(which setxkbmap)"; then
 	setxkbmap -option caps:ctrl_modifier
 fi
 
@@ -11,13 +11,13 @@ fi
 echo "Setting up config symlinks"
 linkables=$( find -H ~/.dotfiles -name "*.ln" )
 for file in $linkables ; do
-    bsname=$(basename $file ".ln")
+    bsname=$(basename "$file" ".ln")
     target="$HOME/.$bsname"
     if [ -e "$target" ]; then
         echo "~${target#$HOME} already exists... Skipping."
     else
         echo "Symlink for $file"
-        ln -s $file $target
+        ln -s "$file" "$target"
     fi
 done
 
