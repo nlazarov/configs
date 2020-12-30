62914f9ad06d6c96fff2c6bcd39dcb2632d14b4a
Nikodim Lazarov <nlazarov@gmail.com>
2020-12-30 20:03:51 +0200

    upgrade myzsh script

--- install/myzsh.sh
+++ install/myzsh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
