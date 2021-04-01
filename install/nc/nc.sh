6405a5943d886331130f051aadff3335c1586bff
Nikodim Lazarov <nlazarov@gmail.com>
2021-04-02 01:47:54 +0300

    nextcloud scripts

--- install/nc/nc.sh
+++ install/nc/nc.sh
@@ -3,7 +3,9 @@
 sudo apt full-upgrade
 sudo apt install apache2
 
-sudo apt install php7.3 php7.3-gd php7.3-sqlite3 php7.3-curl php7.3-zip php7.3-xml php7.3-mbstring php7.3-bz2 php7.3-intl php7.3-smbclient php7.3-imap php7.3-gmp
+sudo a2enmod headers
+
+sudo apt install php7.3 php7.3-gd php7.3-sqlite3 php7.3-curl php7.3-zip php7.3-xml php7.3-mbstring php7.3-bz2 php7.3-intl php7.3-smbclient php7.3-imap php7.3-gmp php7.3-bcmath php-imagick php-apcu
 
 sudo apt install postgresql php-pgsql
 
@@ -13,7 +15,7 @@ sudo -u postgres psql -d template1 -c 'CREATE USER pi CREATEDB;CREATE DATABASE n
 NC_DOMAIN='lazarov.cloud'
 NC_DATA_ROOT=/var/nextcloud
 NC_DATA="$NC_DATA_ROOT"/data
-NC_ROOT=/var/www
+NC_ROOT=/var/www/html
 NC_HOME="$NC_ROOT"/nextcloud
 
 pushd "$NC_ROOT" || exit
@@ -26,6 +28,8 @@ sudo chmod 750 "$NC_HOME"/data
 popd || exit
 
 # Configure nextcloud in apache2
+sudo cp ./root_redir_htaccess $NC_ROOT/.htaccess
+sudo cp ./php_apache2_conf-00-nc.ini /etc/php/7.3/apache2/conf.d/00-nc.ini
 sudo cp ./apache2_sites_available_nextcloud.conf /etc/apache2/sites-available/nextcloud.conf
 sudo a2ensite nextcloud.conf
 sudo systemctl reload apache2
@@ -38,14 +42,15 @@ sudo sed -i "/datadirectory/c\\
     " "$NC_HOME"/config/config.php
 sudo chown -R www-data:www-data $NC_DATA
 
-# update upload max size
-sudo sed -i "/^post_max_size/c\\
-  post_max_size = 1024M
-  " /etc/php/7.3/apache2/php.ini
+function php_ini_update() {
+  sudo sed -i "/^$1/c\\
+    $1 = $2
+    " /etc/php/7.3/apache2/php.ini
+}
 
-sudo sed -i "/^upload_max_filesize/c\\
-  upload_max_filesize = 1024M
-  " /etc/php/7.3/apache2/php.ini
+php_ini_update 'post_max_size' '1024M'
+php_ini_update 'upload_max_filesize' '1024M'
+php_ini_update 'memory_limit' '512M'
 
 sudo systemctl reload apache2
 
@@ -60,3 +65,10 @@ sudo systemctl reload apache2
 
 ./systemd_cron_service.sh "$NC_HOME"
 
+# install nc apps
+popd $NC_HOME || exit
+$NC_APPS=(news notes carnet bookmarks)
+for app in $NC_APPS
+do
+  sudo -u www-data php occ app:install $app
+done
