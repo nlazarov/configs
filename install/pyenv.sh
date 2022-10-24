0f3831310114d823a8500b9955d2c819ec2a08c3
Nikodim Lazarov <nlazarov@gmail.com>
2022-10-24 08:59:52 +0300

    refresh linux setup

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+sudo apt update; sudo apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl https://pyenv.run | bash
