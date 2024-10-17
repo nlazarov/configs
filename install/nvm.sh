daeb11a8916c6b9e8ee8f830a836a8f2c2d246c6
Nikodim Lazarov <nlazarov@gmail.com>
2024-10-17 21:06:36 +0300

    update install scripts

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.40.1/install.sh | bash
