d128ba41f11a4e9835b5df6a0ed1f0eccb7ee1a7
Nikodim Lazarov <nlazarov@protonmail.com>
2025-03-28 11:15:04 +0200

    update wizterm conf

--- config.home/wezterm/wezterm.lua
+++ config.home/wezterm/wezterm.lua
@@ -4,5 +4,8 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = true
-
+config.font = wezterm.font("Source Code Pro for Powerline")
+config.font_size = 13.0
+config.freetype_load_target = 'Light'
+config.freetype_render_target = 'HorizontalLcd'
 return config
