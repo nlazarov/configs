38c461b59e68ddda456202f77d16a015184c45f1
Nikodim Lazarov <nlazarov@protonmail.com>
2025-11-28 16:53:47 +0200

    lazy.nvim experimenting

---
+++ config.home/nvim/lua/plugins/init.lua
@@ -0,0 +1,88 @@
+return {
+'tpope/vim-fugitive',
+'scrooloose/nerdtree',
+'xuyuanp/nerdtree-git-plugin',
+'kien/ctrlp.vim',
+{ 'neoclide/coc.nvim', branch = 'release', },
+{ 'nvim-treesitter/nvim-treesitter', branch = 'master', lazy = false, build = ':TSUpdate' },
+
+'int3/vim-extradite',
+'rbong/vim-flog',
+'airblade/vim-gitgutter',
+'chrisbra/vim-diff-enhanced',
+'fei6409/log-highlight.nvim',
+'comatory/gh-co.nvim',
+
+'tpope/vim-rhubarb',
+'tpope/vim-repeat',
+'tpope/vim-surround',
+'tpope/vim-unimpaired',
+'tpope/vim-eunuch',
+
+'tomtom/tcomment_vim',
+'embear/vim-localvimrc',
+'lambdalisue/suda.vim',
+
+'dense-analysis/ale',
+'gcorne/vim-sass-lint',
+
+'mhinz/vim-startify',
+'sunaku/vim-dasht',
+
+'bling/vim-airline',
+'vim-airline/vim-airline-themes',
+'rakr/vim-one',
+'YorickPeterse/happy_hacking.vim',
+'jansenfuller/crayon',
+'vim-scripts/wombat256.vim',
+'dikiaap/minimalist',
+'w0ng/vim-hybrid',
+'morhetz/gruvbox',
+'sainnhe/everforest',
+'Skullamortis/forest.nvim',
+
+'chiel92/vim-autoformat',
+'terryma/vim-multiple-cursors',
+
+'christoomey/vim-tmux-navigator',
+'mileszs/ack.vim',
+'brooth/far.vim',
+'chrisbra/csv.vim',
+'vim-test/vim-test',
+
+-- TypeScript
+'herringtondarkholme/yats.vim',
+
+'ntpeters/vim-better-whitespace',
+'jiangmiao/auto-pairs',
+'mattn/emmet-vim',
+'stephenway/postcss.vim',
+
+-- snippets
+'SirVer/ultisnips',
+
+-- ReactJS
+ 'pangloss/vim-javascript',
+ 'mxw/vim-jsx',
+
+-- Python
+'puremourning/vimspector',
+'alfredodeza/pytest.vim',
+
+-- Ruby
+'vim-ruby/vim-ruby',
+
+-- yaml
+'pedrohdz/vim-yaml-folds',
+
+-- diff
+'lambdalisue/vim-unified-diff',
+
+-- code-reviews
+'AndrewRadev/diffurcate.vim',
+
+-- AI assist
+'github/copilot.vim',
+'nvim-lua/plenary.nvim',
+'CopilotC-Nvim/CopilotChat.nvim',
+}
